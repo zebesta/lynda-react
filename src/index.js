2f1ce59c8992b41d5ded7071221e30709e9d21fc
zebesta <chris.sebesta@gmail.com>
2017-02-27 12:50:03 -0500

    dynamically building components and parent/child component relations

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { SkiDayCount } from './components/SkiDayCount'
+import { SkiDayList } from './components/SkiDayList'
+
+window.React = React
 
 render(
-  <SkiDayCount total={50}
-                powder = {20}
-                backcountry={10}
-                goal={100}></SkiDayCount>,
+  <SkiDayList days={
+    [
+      {
+        resort: "Squaw Calley",
+        date: new Date("1/2/2016"),
+        powder: true,
+        backcountry: false
+      },
+      {
+        resort: "Vail",
+        date: new Date("1/16/2016"),
+        powder: false,
+        backcountry: false
+      },
+      {
+        resort: "Squaw Valley",
+        date: new Date("2/20/2016"),
+        powder: false,
+        backcountry: true
+      },
+      {
+        resort: "Beaver Creek",
+        date: new Date("2/30/2016"),
+        powder: false,
+        backcountry: false
+      },
+      {
+        resort: "Breckenridge",
+        date: new Date("3/20/2016"),
+        powder: true,
+        backcountry: false
+      }
+    ]
+  }
+  />,
   document.getElementById('react-container')
 )
