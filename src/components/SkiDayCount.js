9399593ca4e723a356b6254c8768db460ab1ea03
zebesta <chris.sebesta@gmail.com>
2017-02-27 12:26:59 -0500

    icons and what not

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -1,34 +1,38 @@
 import React from 'react'
 // import '../stylesheets/ui.scss'
+import Terrain from 'react-icons/lib/md/terrain'
+import SnowFlake from 'react-icons/lib/ti/weather-snow'
+import Calendar from 'react-icons/lib/fa/calendar'
+const percentToDecimal = (decimal) => {
+  return ((decimal * 100) + '%')
+}
 
-export const SkiDayCount = React.createClass({
-  percentToDecimal(decimal) {
-    return ((decimal * 100) + '%')
-  },
-  calcGoalProgress(total, goal) {
-    return this.percentToDecimal(total/goal)
-  },
-  render() {
-    return (
-      <div className="ski-day-count">
-        <div className="total-days">
-          <span>{this.props.total}</span>
-          <span> total ski days</span>
-        </div>
-        <div className="powder-days">
-          <span>{this.props.powder}</span>
-          <span> powder days</span>
-        </div>
-        <div className="backcountry-days">
-          <span>{this.props.backcountry}</span>
-          <span> hiking days</span>
-        </div>
-        <div>
-          <span>
-            {this.calcGoalProgress(this.props.total, this.props.goal)}
-          </span>
-        </div>
-      </div>
-    )
-  }
-})
+
+const calcGoalProgress = (total, goal) =>{
+  return percentToDecimal(total/goal)
+}
+
+export const SkiDayCount = ({total, powder, backcountry, goal}) => (
+  <div className="ski-day-count">
+    <div className="total-days">
+      <span>{total}</span>
+        <Calendar />
+      <span> total ski days</span>
+    </div>
+    <div className="powder-days">
+      <span>{powder}</span>
+        <SnowFlake />
+      <span> powder days</span>
+    </div>
+    <div className="backcountry-days">
+      <span>{backcountry}</span>
+        <Terrain />
+      <span> hiking days</span>
+    </div>
+    <div>
+      <span>
+        {calcGoalProgress(total, goal)}
+      </span>
+    </div>
+  </div>
+)
